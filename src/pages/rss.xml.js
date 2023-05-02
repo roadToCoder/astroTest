c1be51fb30f5122ec53a626e58fc2fb1c22d93c2
roadToCoder <djaffre.prog@gmail.com>
2023-05-02 16:08:22 +0200

    Modification

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function get() {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: 'https://benevolent-meringue-3881c9.netlify.app/',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
